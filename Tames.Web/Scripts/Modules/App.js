1e0ba86e8d228fb973e1b5141bf7c3b1b6902476
Frankenstine Joe <Frankenstine@DESKTOP-KCNC5C4>
2017-12-21 08:44:51 +0500

    Initial

---
+++ Tames.Web/Scripts/Modules/App.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('App', ['ngMaterial']);
+
+
+app.controller('appCtrl', function ($scope) {
+
+    $scope.currentNavItem = 'page1';
+
+    $scope.goto = function (page) {
+        console.log("Goto " + page);
+    }
+
+
+    //Side Nav
+    $scope.imagePath = 'Images/logo2.jpg';
+
+});
